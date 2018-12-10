d85db117c8c50bc8510fe84a48de6e483a33ec9d
joshjin <jinzexi1994@hotmail.com>
2018-12-09 23:26:37 -0500

    Added AlexNet with data preprocessing

--- run_Alex_experiment.py
+++ run_Alex_experiment.py
@@ -11,12 +11,11 @@ from torch.autograd import Variable
 from torchvision import transforms
 
 # an.load_data_for_alex('X.npy', 'y.npy')
-
-# X_train, y_train, X_test, y_test = an.extract_data(os.path.join("519_refined_data", "data"))
-# np.save('X_train_small.npy', X_train)
-# np.save('y_train_small.npy', y_train)
-# np.save('X_test_small.npy', X_test)
-# np.save('y_test_small.npy', y_test)
+X_train, y_train, X_test, y_test = an.extract_data(os.path.join("519_refined_data", "data"))
+np.save('X_train_small.npy', X_train)
+np.save('y_train_small.npy', y_train)
+np.save('X_test_small.npy', X_test)
+np.save('y_test_small.npy', y_test)
 
 
 
